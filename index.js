3230a9dd956ea754968bf102fa6bdfe1d0ffff1c
Carly Sierra <carlylwomack@gmail.com>
2017-10-05 18:34:23 -0500

    added html and js

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const SPOONACULAR_RECIPE_SEARCH_URL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search"
 const SPOONACULAR_INGREDIENT_SEARCH_URL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients"
 
-function getApiData() {
-    const query = {
+function getApiData(searchTerm, callback) {
+    const searchQuery = {
         "X-Mashape-Key": "BBoWkCTDGBmshCZoX3ZR3kLWD1c7p11PDxljsnGpErhBJzHGm7",
-        accept: application/json
-    }
+        accept: application/json,
+        ingredients: `${searchTerm}`,
+        number: 25
+    };
+    $.getJSON(SPOONACULAR_INGREDIENT_SEARCH_URL,searchQuery, callback);
 }
+
+function renderResults(result) {
+    return ''
+};
+
+function displayRecipes(data) {
+
+};
+
+function watchSubmit() {
+
+};
