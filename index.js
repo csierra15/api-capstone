4cf11f95ce817d17c6e1bd19daedd2fc7342acfc
Carly Sierra <carlylwomack@gmail.com>
2017-10-10 13:33:02 -0500

    updated js

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 'use strict';
 
 const SPOONACULAR_INGREDIENT_SEARCH_URL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients";
-const   SPOONACULAR_RECIPE_INFO_URL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/informationBulk";
+const SPOONACULAR_RECIPE_INFO_URL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/informationBulk";
+const recipes = [];
 
-function getApiData(searchTerm, ids, callback) {
+function getApiData(searchTerm, callback) {
     const headers = {
       "X-Mashape-Key": "JsnpQNRGfymshHh9jahYd6uvMBnap11yi26jsnT4SP8OOkG2Vh",
       Accept: "application/json"};
       
     const searchQuery = {
       ingredients: `${searchTerm}`,
-      number: 25
+      number: 5
     };
 
-      $.ajax({
-          method: "GET",
-          url: SPOONACULAR_INGREDIENT_SEARCH_URL,
-          data: searchQuery,
-          headers: headers
-      }).done(function(data){
-          let ids = data.map(r => r.id);
-            $.ajax({
-                method: "GET",
-                url: SPOONACULAR_RECIPE_INFO_URL,
-                data: ids.join(','),
-                headers: headers
-          }).done(callback)
+    $.ajax({
+        method: "GET",
+        url: SPOONACULAR_INGREDIENT_SEARCH_URL,
+        data: searchQuery,
+        headers: headers
+    }).done(function(data){
+        let ids = data.map(r => r.id);
+        $.ajax({
+            method: "GET",
+            url: SPOONACULAR_RECIPE_INFO_URL,
+            data: {ids: ids.join(',')},
+            headers: headers
+        }).done(function(recipes){
+            callback(data);
         });
-        
+    });
+
     console.log('getApiData ran');
 }
 
@@ -35,15 +38,15 @@ function getApiData(searchTerm, ids, callback) {
 function renderResults(result) {
   console.log('renderResults ran');
   return `
-    <ul>
-      <li>
-        <img src="${result.image}" alt="Picture of ${result.title}">
-        <p>${result.title}</p>
-      </li>
-    </ul>
-    <div class="js-recipie-info">
-      <p>by ${result.sourceName}</p>
-      <a href="${result.sourceUrl}">See full recipe</a>
+    <div class="recipe" data-id="${result.id}">
+        <div>
+            <img src="${result.image}" alt="Picture of ${result.title}">
+            <p>${result.title}</p>
+        </div>
+        <div class="js-recipe-info">
+            <p>by ${result.sourceName}</p>
+            <a href="${result.sourceUrl}">See full recipe</a>
+        </div>
     </div>
   `;
 }
@@ -62,6 +65,11 @@ function watchSubmit() {
       const search = searchTarget.val();
       searchTarget.val('');
       getApiData(search, displayRecipes);
+    });
+
+    $('.js-search-results').on('click', '.recipe', event => {
+        let id = $(event.target).closest('.recipe').data('id');
+        let recipe = recipes.find(r => r.id == id);
     });
 }
 
