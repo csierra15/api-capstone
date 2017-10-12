15fb5cc1d7ed4c794ccbbb0fcd14513ca5197a04
Carly Sierra <carlylwomack@gmail.com>
2017-10-12 15:15:02 -0500

    condensed css files, added back to top button, polished styles

--- index.js
+++ index.js
@@ -48,10 +48,7 @@ function renderRecipeInfo(result) {
         </div>
     </div>`;
 }
-//html to be added later
-//<div class="js-more-recipe-info">
-//<button type="submit" class="exit-popup" hidden>x</button>
-//</div>
+
 
 function displayRecipes(data) {
   console.log('displayRecipes ran');
@@ -68,17 +65,89 @@ function watchSubmitSearch() {
       searchTarget.val('');
       getApiData(search, displayRecipes);
     });
+    
+      'use strict';
 
-    //$('.js-search-results').on('click', 'img', event => {
-        //let id = $(event.target).closest('.recipe').data('id');
-        //let recipe = recipes.find(r => r.id == id);
-        
-        //$('.js-more-recipe-info').show();
-          //$('.exit-popup').on('click', event => {
-            //$('.js-more-recipe-info').hide();
-          //});
-        //console.log(recipe);
-    //});
+const SPOONACULAR_INGREDIENT_SEARCH_URL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients";
+const SPOONACULAR_RECIPE_INFO_URL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/informationBulk";
+let recipes = [];
+
+function getApiData(searchTerm, callback) {
+    const headers = {
+      "X-Mashape-Key": "JsnpQNRGfymshHh9jahYd6uvMBnap11yi26jsnT4SP8OOkG2Vh",
+      Accept: "application/json"};
+      
+    const searchQuery = {
+      ingredients: `${searchTerm}`,
+      number: 30
+    };
+
+    $.ajax({
+        method: "GET",
+        url: SPOONACULAR_INGREDIENT_SEARCH_URL,
+        data: searchQuery,
+        headers: headers
+    }).done(function(data){
+        let ids = data.map(r => r.id);
+        $.ajax({
+            method: "GET",
+            url: SPOONACULAR_RECIPE_INFO_URL,
+            data: {ids:ids.join(',')},
+            headers: headers
+        }).done(function(data) {
+            console.log(recipes);
+            recipes = data;
+            callback(data);
+        });
+    });
+
+    console.log('getApiData ran');
+}
+
+
+function renderRecipeInfo(result) {
+  console.log('renderRecipeInfo ran');
+  return `
+    <div class="recipe" data-id="${result.id}">
+        <div class="recipe-src-info">
+            <p>${result.title}</p>
+            <img src="${result.image}" class="recipe-img" alt="Picture of ${result.title}">
+            <a href="${result.sourceUrl}" target="_blank">See full recipe at ${result.sourceName}</a>
+        </div>
+    </div>`;
+}
+
+
+function displayRecipes(data) {
+  console.log('displayRecipes ran');
+  const results = data.map((item, index) => renderRecipeInfo(item));
+    $('.js-search-results').html(results);
+}
+
+function watchSubmitSearch() {
+  console.log('watchSubmitSearch ran');
+    $('.js-search-form').submit(event => {
+      event.preventDefault();
+      const searchTarget = $(event.currentTarget).find('.js-search-bar');
+      const search = searchTarget.val();
+      searchTarget.val('');
+      getApiData(search, displayRecipes);
+    });
+    
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+          $('#top-btn').fadeIn();
+        } else {
+          $('#top-btn').fadeOut();
+        }
+      });
+
+      $("#top-btn").click(function(event) {
+        event.preventDefault();
+        $("main").animate({ scrollTop: 0 }, "fast");
+        return false;
+      });
+      }
 }
 
 $(watchSubmitSearch);
