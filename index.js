c0e1dc94db404b1f219a43046702fcc36fa6b9d1
Carly Sierra <carlylwomack@gmail.com>
2017-10-12 15:40:19 -0500

    fixed incorrect js

--- index.js
+++ index.js
@@ -5,33 +5,33 @@ const SPOONACULAR_RECIPE_INFO_URL = "https://spoonacular-recipe-food-nutrition-v
 let recipes = [];
 
 function getApiData(searchTerm, callback) {
-    const headers = {
-      "X-Mashape-Key": "JsnpQNRGfymshHh9jahYd6uvMBnap11yi26jsnT4SP8OOkG2Vh",
-      Accept: "application/json"};
+  const headers = {
+    "X-Mashape-Key": "JsnpQNRGfymshHh9jahYd6uvMBnap11yi26jsnT4SP8OOkG2Vh",
+    Accept: "application/json"};
       
-    const searchQuery = {
-      ingredients: `${searchTerm}`,
-      number: 30
-    };
+  const searchQuery = {
+    ingredients: `${searchTerm}`,
+    number: 30
+  };
 
-    $.ajax({
+  $.ajax({
+    method: "GET",
+    url: SPOONACULAR_INGREDIENT_SEARCH_URL,
+    data: searchQuery,
+    headers: headers
+  }).done(function(data){
+      let ids = data.map(r => r.id);
+      $.ajax({
         method: "GET",
-        url: SPOONACULAR_INGREDIENT_SEARCH_URL,
-        data: searchQuery,
+        url: SPOONACULAR_RECIPE_INFO_URL,
+        data: {ids:ids.join(',')},
         headers: headers
-    }).done(function(data){
-        let ids = data.map(r => r.id);
-        $.ajax({
-            method: "GET",
-            url: SPOONACULAR_RECIPE_INFO_URL,
-            data: {ids:ids.join(',')},
-            headers: headers
-        }).done(function(data) {
-            console.log(recipes);
-            recipes = data;
-            callback(data);
-        });
-    });
+      }).done(function(data) {
+          console.log(recipes);
+          recipes = data;
+          callback(data);
+      });
+  });
 
     console.log('getApiData ran');
 }
@@ -41,79 +41,11 @@ function renderRecipeInfo(result) {
   console.log('renderRecipeInfo ran');
   return `
     <div class="recipe" data-id="${result.id}">
-        <div class="recipe-src-info">
-            <p>${result.title}</p>
-            <img src="${result.image}" class="recipe-img" alt="Picture of ${result.title}">
-            <a href="${result.sourceUrl}" target="_blank">See full recipe at ${result.sourceName}</a>
-        </div>
-    </div>`;
-}
-
-
-function displayRecipes(data) {
-  console.log('displayRecipes ran');
-  const results = data.map((item, index) => renderRecipeInfo(item));
-    $('.js-search-results').html(results);
-}
-
-function watchSubmitSearch() {
-  console.log('watchSubmitSearch ran');
-    $('.js-search-form').submit(event => {
-      event.preventDefault();
-      const searchTarget = $(event.currentTarget).find('.js-search-bar');
-      const search = searchTarget.val();
-      searchTarget.val('');
-      getApiData(search, displayRecipes);
-    });
-    
-      'use strict';
-
-const SPOONACULAR_INGREDIENT_SEARCH_URL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients";
-const SPOONACULAR_RECIPE_INFO_URL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/informationBulk";
-let recipes = [];
-
-function getApiData(searchTerm, callback) {
-    const headers = {
-      "X-Mashape-Key": "JsnpQNRGfymshHh9jahYd6uvMBnap11yi26jsnT4SP8OOkG2Vh",
-      Accept: "application/json"};
-      
-    const searchQuery = {
-      ingredients: `${searchTerm}`,
-      number: 30
-    };
-
-    $.ajax({
-        method: "GET",
-        url: SPOONACULAR_INGREDIENT_SEARCH_URL,
-        data: searchQuery,
-        headers: headers
-    }).done(function(data){
-        let ids = data.map(r => r.id);
-        $.ajax({
-            method: "GET",
-            url: SPOONACULAR_RECIPE_INFO_URL,
-            data: {ids:ids.join(',')},
-            headers: headers
-        }).done(function(data) {
-            console.log(recipes);
-            recipes = data;
-            callback(data);
-        });
-    });
-
-    console.log('getApiData ran');
-}
-
-
-function renderRecipeInfo(result) {
-  console.log('renderRecipeInfo ran');
-  return `
-    <div class="recipe" data-id="${result.id}">
-        <div class="recipe-src-info">
-            <p>${result.title}</p>
-            <img src="${result.image}" class="recipe-img" alt="Picture of ${result.title}">
-            <a href="${result.sourceUrl}" target="_blank">See full recipe at ${result.sourceName}</a>
-        </div>
+      <div class="recipe-src-info">
+        <p>${result.title}</p>
+        <img src="${result.image}" class="recipe-img" alt="Picture of ${result.title}">
+        <a href="${result.sourceUrl}" target="_blank">See full recipe at ${result.sourceName}</a>
+      </div>
     </div>`;
 }
 
@@ -147,7 +79,6 @@ function watchSubmitSearch() {
         $("main").animate({ scrollTop: 0 }, "fast");
         return false;
       });
-      }
 }
 
 $(watchSubmitSearch);
